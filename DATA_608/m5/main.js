e39b278a5a0d0cd9842b0e3ee945f7cbbedc5b83
Richard Zheng <61770465+schoolkidrich@users.noreply.github.com>
2021-11-08 11:15:53 -0500

    Add files via upload

--- DATA_608/m5/main.js
+++ DATA_608/m5/main.js
@@ -29,3 +29,76 @@ function getTables(num){
   }
 	return (tables)
 }
+
+
+function processData(data){
+	let presidents = {}
+	for (let i in data){
+		let president = data[i]
+		presidents[president.Name] = [president.Height, president.Weight]
+		}
+  createTable(presidents);
+  createLabel(Object.keys(presidents));
+  changeSelection(presidents);
+	}
+
+
+function createTable(dict){
+	let entries = document.getElementById('presidentTable')
+	let table = '<table>\n<tr><td>Name</td><td>Height</td><td>Weight</td></tr>\n'
+
+	for (let i in dict){
+		n = i;
+		h = dict[i][0]
+		w = dict[i][1]
+		table = table + 
+			'<tr><td>'+ n +
+			'</td><td>'+ h +
+			'</td><td>'+ w + 
+			'</td></tr>\n'
+	}
+	entries.innerHTML = table + '</table>'
+}
+
+
+function loadData(){
+	Plotly.d3.csv('https://raw.githubusercontent.com/schoolkidrich/schoolkidrich.github.io/main/presidents.csv',
+	function(data){ processData(data);
+  })
+ }
+ 
+ 
+function createLabel(data){
+	 Plotly.d3
+    .select('#options')
+    .selectAll('option')
+    .data(data)
+    .enter()
+    .append('option')
+    .text(function(d) {
+      return d;
+    })
+    .attr('value', function(d) {
+      return d;
+    });
+}
+
+
+
+function setTraits(data, val){
+	let traits = document.getElementById('traits')
+  let text = 'Height: ' + data[val][0] + '\nWeight: ' + data[val][1] 
+  traits.innerHTML = text
+}
+
+
+
+function changeSelection(data){
+	Plotly.d3.select('#options').on('change', function() {
+			let newVal = Plotly.d3.event.target.value
+    	setTraits(data, newVal)
+  	})
+ }
+
+
+loadData()
